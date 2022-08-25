726c4ed1e0773d2aa835ae1c39813ec705d99a61
Iramar Ferreira <iramarbsi@gmail.com>
2022-08-25 14:56:19 -0300

    adicionado camada de repository

---
+++ src/main/java/com/residenciaTst/AtividadePratica/repository/PautaRepository.java
@@ -0,0 +1,11 @@
+package com.residenciaTst.AtividadePratica.repository;
+
+import com.residenciaTst.AtividadePratica.model.Pauta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PautaRepository extends JpaRepository <Pauta, UUID> {
+}
