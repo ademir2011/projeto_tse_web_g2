b405cf4fc0b983d910655cd8e56c38d45e45c720
Iramar Ferreira <iramarbsi@gmail.com>
2022-08-25 23:20:37 -0300

    Adicionado camada de serviço

---
+++ src/main/java/com/residenciaTst/AtividadePratica/service/PautaService.java
@@ -0,0 +1,51 @@
+package com.residenciaTst.AtividadePratica.service;
+
+import com.residenciaTst.AtividadePratica.model.Pauta;
+import com.residenciaTst.AtividadePratica.model.Processo;
+import com.residenciaTst.AtividadePratica.repository.PautaRepository;
+import com.residenciaTst.AtividadePratica.repository.ProcessoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PautaService {
+
+    @Autowired
+    PautaRepository pautaRepository;
+
+    public Pauta salvar(Pauta pauta){
+        return pautaRepository.save(pauta);
+    }
+
+    public List<Pauta> listarTodos(){
+        return pautaRepository.findAll();
+    }
+
+    public Pauta listarPeloId(UUID id){
+        if(pautaRepository.findById(id).isPresent()){
+            return pautaRepository.findById(id).get();
+        }else{
+            return null;
+        }
+    }
+
+    public Pauta atualizar(UUID id, Pauta processo){
+        if(pautaRepository.findById(id).isPresent()){
+            return pautaRepository.save(processo);
+        }else{
+            return null;
+        }
+    }
+
+    public boolean deletarPeloId(UUID id){
+        if(pautaRepository.findById(id).isPresent()){
+            pautaRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/residenciaTst/AtividadePratica/service/ProcessoService.java
@@ -0,0 +1,51 @@
+package com.residenciaTst.AtividadePratica.service;
+
+import com.residenciaTst.AtividadePratica.model.Processo;
+import com.residenciaTst.AtividadePratica.repository.ProcessoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProcessoService {
+
+    @Autowired
+    ProcessoRepository processoRepository;
+
+    public Processo salvar(Processo processo){
+        return processoRepository.save(processo);
+    }
+
+    public List<Processo> listarTodos(){
+        return processoRepository.findAll();
+    }
+
+    public Processo listarPeloId(UUID id){
+        if(processoRepository.findById(id).isPresent()){
+            return processoRepository.findById(id).get();
+        }else{
+            return null;
+        }
+    }
+
+    public Processo atualizar(UUID id, Processo processo){
+        if(processoRepository.findById(id).isPresent()){
+            return processoRepository.save(processo);
+        }else{
+            return null;
+        }
+    }
+
+    public boolean deletarPeloId(UUID id){
+        if(processoRepository.findById(id).isPresent()){
+            processoRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+}
